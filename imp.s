8c264686553931c5d22cf02c27e422416c312565
Cytla24 <ojuba2013@gmail.com>
2019-10-26 21:11:36 -0400

    found M and N from my ID number + comments

--- imp.s
+++ imp.s
@@ -4,4 +4,8 @@
 .text
 main:
 		addi $t0, $zero, 02837763   # int $t0 = 02837763
-		addii $t1, $zero, 11		# int $t1 = 11
+		addi $t1, $zero, 11			# int $t1 = 11
+		div $t0, $t1 				# divide t0/t1
+		mfhi $t3					# remainder = t3
+		addi $s0, $t3, 26 			# N = s0
+		subi $s1, $s0, 10 			# M = s1
