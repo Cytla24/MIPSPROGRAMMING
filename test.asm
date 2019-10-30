79032933189d71c439dfa04830f4bfbd3faf7be0
Cytla24 <ojuba2013@gmail.com>
2019-10-30 18:13:28 -0400

    changed main file back to imp.s and removed test.asm

--- test.asm
+++ test.asm
@@ -1,11 +1,12 @@
 .data
-Ask_Input:. asciiz "\Please Enter a String\n"
+Ask_Input:. asciiz "Please Enter a String\n"
 .text
 main:
-	addi $t0, $zero, 02837763   # int $t0 = 02837763
+	addi $t0, $zero, 02837763  		# int $t0 = 02837763
 	addi $t1, $zero, 11			# int $t1 = 11
 	div $t0, $t1 				# divide t0/t1
 	mfhi $t3					# remainder = t3
 	addi $s0, $t3, 26 			# N = s0
-	subi $s1, $s0, 10 			# M = s1
+	addi $t9, $zero, 10
+	sub $s1, $s0, $t9			# M = s1
 	Syscall
