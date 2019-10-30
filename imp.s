6998a43fe3b4be4a912a149264e64794b17a44b6
Cytla24 <ojuba2013@gmail.com>
2019-10-30 18:19:21 -0400

    updated add statement to value < 2^15 and included comments.also replaced subi with sub

--- imp.s
+++ imp.s
@@ -7,11 +7,13 @@ Ask_Input:.asciiz "\Please Enter a String\n"
 .text
 
 main:
-	addi $t0, $zero, 02837763
-	addi $t1, $zero, 11
-	div $t0, $t1
-	mfhi $t3
-	addi $s0, $t3, 26
+	lui $t8, 43
+	ori $t8, $t8, 19715
+	add $t0, $zero, $t8				# int $t0 = 02837763
+	addi $t1, $zero, 11				# int $t1 = 11
+	div $t0, $t1					# divide t0/t1
+	mfhi $t3						# remainder = t3
+	addi $s0, $t3, 26				# N = s0
 	addi $t9, $zero, 10
-	sub $s1, $s0, $t9
+	sub $s1, $s0, $t9				# M = s1
 
